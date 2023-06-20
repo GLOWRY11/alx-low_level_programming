c975e21684906ee7877d584a3e94a1f48e95ca62
GLOWRY11 <achirmarouane2@gmail.com>
2023-06-20 04:34:55 -0700

    test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,23 +5,23 @@
  * @n: the number to check
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
- * and prints the corresponding sign character
+ *         and prints the corresponding sign character
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
-    else
-    {
-        _putchar('-');
-        return -1;
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
